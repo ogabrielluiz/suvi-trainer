6b218e401be00c3fc58aa2418942f81b90e4cb60
jmbhughes <hughes.jmb@gmail.com>
2018-07-25 10:28:46 -0600

    included description in setup.py corrected url in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup, find_packages
 
+with open("Readme.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='suvitrainer',
-      version='1.0.1',
+      version='1.0.2',
       description='Training tool for SUVI thematic map machine learning',
-      url='https://github.com/jmbhughes/suvitrainer',
+      url='https://github.com/jmbhughes/suvi-trainer',
       author='J. Marcus Hughes',
       author_email='hughes.jmb@gmail.com',
       license='MIT',
@@ -12,6 +15,8 @@ setup(name='suvitrainer',
       # test_suite='nose.collector',
       # tests_require=['nose'],
       scripts=["run.py"],
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       install_requires=['astropy',
                         'numpy',
                         'sunpy',
